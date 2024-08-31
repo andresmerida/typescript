adb54ef65a5a2f6358b49a24dc3e2eaac56c966b
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2024-08-30 23:22:41 -0400

    adding new features

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -441,3 +441,32 @@ let arrayNumbers = createArray<number>(10, 10);
 console.log(arrayStrings);
 console.log(arrayNumbers);
 
+const url = 'https://www.course-api.com/react-tours-project';
+type Tour = {
+    id: string;
+    name: string;
+    info: string;
+    image: string;
+    price: string;
+};
+
+async function fetchData(url: string): Promise<Tour[]> {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status:${response.status}`);
+        }
+        const data: Tour[] = await response.json();
+
+        return data;
+    } catch(error) {
+        const errorMsg = error instanceof Error ? error.message : 'there was an error...';
+        console.log(errorMsg);
+        return [];
+    }
+}
+
+const tours = await fetchData(url);
+tours.map((tour) => {
+    console.log(tour.name);
+});
